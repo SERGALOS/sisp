a67052675af7bc3f06394cd60c977174d9f0ab98
SERGALOS <134278543+SERGALOS@users.noreply.github.com>
2023-11-17 21:37:56 +1000

    начал z3

---
+++ z3/z3.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int boxWithApples = 15; // количество ящиков на складе
+	int amountBoxes = 0; // количество отгружаемых ящиков
+}
