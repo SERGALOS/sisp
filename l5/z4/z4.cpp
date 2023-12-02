a6f36f600ede1da9936064acc68617cd6bb76e52
SERGALOS <134278543+SERGALOS@users.noreply.github.com>
2023-12-02 12:32:06 +1000

    l5z4

--- l5/z4/z4.cpp
+++ l5/z4/z4.cpp
@@ -1,1 +1,10 @@
-﻿#include
+﻿#include <iostream>
+using namespace std;
+
+int main(void) {
+	double** p = 0;
+	*(*(p = new double*) = new double) = 2;
+	cout << **p << endl;
+	delete* p;
+	delete p;
+}
---
+++ l5/z5/z5.cpp
@@ -0,0 +1,6 @@
+﻿#include <iostream>
+using namespace std;
+double* giveMemoyToArr(double* startPtr, int sizeOfArr);
+void fillArr(double* ptrArr, int sizeOfArr);
+void showArr(double* ptrArr, int sizeOfArr);
+double* freeMemory(doble* ptrArr,);
