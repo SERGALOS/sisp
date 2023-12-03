5caa04a9751c100da3af0fa699bb3b6aedef82be
SERGALOS <134278543+SERGALOS@users.noreply.github.com>
2023-12-03 05:28:02 +0000

    test

---
+++ l5/ztest/ztest.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void fillAndShowArry(int arrayForFilling[], int size);
+
+int main()
+{
+	const int SiZE1 = 8;
+	const int SIZE2 = 14;
+
+	int arrayForFilling1[SiZE1] = {};
+	int arrayForFilling2[SIZE2] = {};
+	fillAndShowArry(arrayForFilling1, SiZE1);
+	fillAndShowArry(arrayForFilling2, SIZE2);
+	return 0;
+}
+
+void fillAndShowArry(int arrayForFilling[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		arrayForFilling[i] = i + 1;
+		cout << arrayForFilling[i] << "  ";
+	}
+	cout << endl;
+}
